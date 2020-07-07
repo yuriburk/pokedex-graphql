27d4fdf464e6b40fe1b247a710b453e90a5787da
Yuri Burk <yuriburk@gmail.com>
2020-07-06 23:06:53 -0300

    chore: error -> not found

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,18 @@ import { Switch, Route } from 'react-router-dom';
 import Pokedex from 'pages/Pokedex';
 import PokemonDetail from 'pages/PokemonDetail';
 
-const Error = () => <div>Error</div>;
+const NotFound = () => (
+  <div>
+    <h1>404</h1>
+    <p>NotFound</p>
+  </div>
+);
 
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/" component={Pokedex} />
     <Route path="/edit/:id" component={PokemonDetail} />
-    <Route path="*" component={Error} />
+    <Route path="*" component={NotFound} />
   </Switch>
 );
 
